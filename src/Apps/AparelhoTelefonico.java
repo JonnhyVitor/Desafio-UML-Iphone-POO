48e568bdce34492937a09edfbe25b717f5bd69d4
jonnhyvitor <jbs.tek.net@gmail.com>
2024-06-05 20:52:00 -0300

    corrigindo

--- src/Apps/AparelhoTelefonico.java
+++ src/Apps/AparelhoTelefonico.java
@@ -1,6 +1,6 @@
  package Apps;
 
-  public interface ApareloTelefonico {
+  public interface AparelhoTelefonico {
   public void ligar(String numero);
   public void atender();
   public void iniciarCorreioVoz();
